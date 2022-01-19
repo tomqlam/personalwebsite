cdff20edd635c2dbffb581ff75c1780e2d0ff813
Duy Lam <tomqlam@gmail.com>
2022-01-19 02:20:02 -0800

    Google Cloud Deployment Stuff

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,36 +1,26 @@
 <template>
   <div class="about">
-    <div class="intro">
-      <h1>About Me</h1>
-      <h2>
-        I am a full-stack web developer with a passion for creating beautiful,
-        functional, and responsive websites.
-      </h2>
+    <div class="main">
+      <div class="avatar">
+        <img
+          src="../assets/profile.png"
+          alt="Circle Image"
+          class="img-raised rounded-circle img-fluid"
+        />
+      </div>
+      <div class="name">
+        <h3 class="title">Duy Lam</h3>
+        <h6>Software Engineer</h6>
+      </div>
+      <div class="description text-center">
+        <p>
+          I am a software engineer with a passion for building web applications
+          and machine learning. I am currently pursuing a degree in computer
+          science at Harvey Mudd College in Claremont, California.
+        </p>
+      </div>
     </div>
   </div>
 </template>
 
-<style scoped>
-.about {
-  z-index: 1;
-}
-.intro {
-  position: absolute;
-  margin-left: 0vw;
-  margin-top: 100px;
-}
-h1 {
-  font-size: 4vw;
-  font-weight: bold;
-  color: rgb(255, 255, 255);
-  text-shadow: 0 0 10px rgba(255, 0, 255, 0.5), 0 0 40px rgba(255, 0, 255, 0.5),
-    0 0 80px rgba(255, 0, 255, 0.5), 0 0 160px rgba(255, 0, 255, 0.5);
-}
-h2 {
-  font-size: 3vw;
-  font-weight: bold;
-  color: rgb(255, 255, 255);
-  text-shadow: 0 0 10px rgba(255, 0, 255, 0.5), 0 0 40px rgba(255, 0, 255, 0.5),
-    0 0 80px rgba(255, 0, 255, 0.5), 0 0 160px rgba(255, 0, 255, 0.5);
-}
-</style>
+<style src="../styles/About.css"></style>
